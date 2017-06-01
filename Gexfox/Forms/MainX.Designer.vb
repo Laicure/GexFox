6cb13d88e1e00d181951f5ba2cee3b3294dd98d8
Laicure <Laicure@outlook.com>
2017-06-01 09:10:50 +0800

    initial release

--- Gexfox/Forms/MainX.Designer.vb
+++ Gexfox/Forms/MainX.Designer.vb
@@ -22,8 +22,184 @@ Partial Class MainX
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "MainX"
-    End Sub
+		Me.panGex = New System.Windows.Forms.Panel()
+		Me.panConts = New System.Windows.Forms.Panel()
+		Me.tbOpac = New System.Windows.Forms.TrackBar()
+		Me.txUrl = New System.Windows.Forms.TextBox()
+		Me.lbLoad = New System.Windows.Forms.Label()
+		Me.lbForward = New System.Windows.Forms.Label()
+		Me.lbReload = New System.Windows.Forms.Label()
+		Me.lbBack = New System.Windows.Forms.Label()
+		Me.butMin = New System.Windows.Forms.Button()
+		Me.lbUA = New System.Windows.Forms.Label()
+		Me.panConts.SuspendLayout()
+		CType(Me.tbOpac, System.ComponentModel.ISupportInitialize).BeginInit()
+		Me.SuspendLayout()
+		'
+		'panGex
+		'
+		Me.panGex.Dock = System.Windows.Forms.DockStyle.Fill
+		Me.panGex.Location = New System.Drawing.Point(1, 23)
+		Me.panGex.Name = "panGex"
+		Me.panGex.Size = New System.Drawing.Size(332, 312)
+		Me.panGex.TabIndex = 0
+		Me.panGex.Visible = False
+		'
+		'panConts
+		'
+		Me.panConts.Controls.Add(Me.txUrl)
+		Me.panConts.Controls.Add(Me.lbUA)
+		Me.panConts.Controls.Add(Me.butMin)
+		Me.panConts.Controls.Add(Me.lbBack)
+		Me.panConts.Controls.Add(Me.lbReload)
+		Me.panConts.Controls.Add(Me.lbForward)
+		Me.panConts.Controls.Add(Me.tbOpac)
+		Me.panConts.Dock = System.Windows.Forms.DockStyle.Top
+		Me.panConts.Location = New System.Drawing.Point(1, 1)
+		Me.panConts.Name = "panConts"
+		Me.panConts.Size = New System.Drawing.Size(332, 22)
+		Me.panConts.TabIndex = 1
+		'
+		'tbOpac
+		'
+		Me.tbOpac.BackColor = System.Drawing.Color.White
+		Me.tbOpac.Dock = System.Windows.Forms.DockStyle.Right
+		Me.tbOpac.Location = New System.Drawing.Point(282, 0)
+		Me.tbOpac.Maximum = 100
+		Me.tbOpac.Minimum = 10
+		Me.tbOpac.Name = "tbOpac"
+		Me.tbOpac.Size = New System.Drawing.Size(50, 22)
+		Me.tbOpac.TabIndex = 1
+		Me.tbOpac.TabStop = False
+		Me.tbOpac.TickFrequency = 2
+		Me.tbOpac.TickStyle = System.Windows.Forms.TickStyle.None
+		Me.tbOpac.Value = 75
+		'
+		'txUrl
+		'
+		Me.txUrl.BackColor = System.Drawing.Color.White
+		Me.txUrl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+		Me.txUrl.Dock = System.Windows.Forms.DockStyle.Fill
+		Me.txUrl.ForeColor = System.Drawing.Color.Black
+		Me.txUrl.Location = New System.Drawing.Point(0, 0)
+		Me.txUrl.Margin = New System.Windows.Forms.Padding(3, 3, 0, 3)
+		Me.txUrl.Name = "txUrl"
+		Me.txUrl.Size = New System.Drawing.Size(194, 22)
+		Me.txUrl.TabIndex = 0
+		Me.txUrl.TabStop = False
+		'
+		'lbLoad
+		'
+		Me.lbLoad.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+		Me.lbLoad.Font = New System.Drawing.Font("Gabriola", 36.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.lbLoad.Location = New System.Drawing.Point(11, 36)
+		Me.lbLoad.Margin = New System.Windows.Forms.Padding(10)
+		Me.lbLoad.Name = "lbLoad"
+		Me.lbLoad.Size = New System.Drawing.Size(312, 289)
+		Me.lbLoad.TabIndex = 0
+		Me.lbLoad.Text = "Loading..."
+		Me.lbLoad.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+		'
+		'lbForward
+		'
+		Me.lbForward.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+		Me.lbForward.Dock = System.Windows.Forms.DockStyle.Right
+		Me.lbForward.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+		Me.lbForward.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.lbForward.Location = New System.Drawing.Point(260, 0)
+		Me.lbForward.Name = "lbForward"
+		Me.lbForward.Size = New System.Drawing.Size(22, 22)
+		Me.lbForward.TabIndex = 2
+		Me.lbForward.Text = "F"
+		Me.lbForward.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+		'
+		'lbReload
+		'
+		Me.lbReload.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+		Me.lbReload.Dock = System.Windows.Forms.DockStyle.Right
+		Me.lbReload.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+		Me.lbReload.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.lbReload.Location = New System.Drawing.Point(238, 0)
+		Me.lbReload.Name = "lbReload"
+		Me.lbReload.Size = New System.Drawing.Size(22, 22)
+		Me.lbReload.TabIndex = 3
+		Me.lbReload.Text = "R"
+		Me.lbReload.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+		'
+		'lbBack
+		'
+		Me.lbBack.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+		Me.lbBack.Dock = System.Windows.Forms.DockStyle.Right
+		Me.lbBack.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+		Me.lbBack.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.lbBack.Location = New System.Drawing.Point(216, 0)
+		Me.lbBack.Margin = New System.Windows.Forms.Padding(0, 0, 3, 0)
+		Me.lbBack.Name = "lbBack"
+		Me.lbBack.Size = New System.Drawing.Size(22, 22)
+		Me.lbBack.TabIndex = 4
+		Me.lbBack.Text = "B"
+		Me.lbBack.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+		'
+		'butMin
+		'
+		Me.butMin.DialogResult = System.Windows.Forms.DialogResult.Cancel
+		Me.butMin.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+		Me.butMin.Location = New System.Drawing.Point(-130, 1)
+		Me.butMin.Name = "butMin"
+		Me.butMin.Size = New System.Drawing.Size(123, 25)
+		Me.butMin.TabIndex = 2
+		Me.butMin.TabStop = False
+		Me.butMin.Text = "Emergency Minimize"
+		Me.butMin.UseVisualStyleBackColor = True
+		'
+		'lbUA
+		'
+		Me.lbUA.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+		Me.lbUA.Dock = System.Windows.Forms.DockStyle.Right
+		Me.lbUA.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+		Me.lbUA.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.lbUA.Location = New System.Drawing.Point(194, 0)
+		Me.lbUA.Margin = New System.Windows.Forms.Padding(0, 0, 3, 0)
+		Me.lbUA.Name = "lbUA"
+		Me.lbUA.Size = New System.Drawing.Size(22, 22)
+		Me.lbUA.TabIndex = 5
+		Me.lbUA.Text = "M"
+		Me.lbUA.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+		'
+		'MainX
+		'
+		Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+		Me.BackColor = System.Drawing.Color.White
+		Me.CancelButton = Me.butMin
+		Me.ClientSize = New System.Drawing.Size(334, 336)
+		Me.Controls.Add(Me.lbLoad)
+		Me.Controls.Add(Me.panGex)
+		Me.Controls.Add(Me.panConts)
+		Me.DoubleBuffered = True
+		Me.Font = New System.Drawing.Font("Segoe UI", 8.25!)
+		Me.ForeColor = System.Drawing.Color.Black
+		Me.MaximizeBox = False
+		Me.MinimumSize = New System.Drawing.Size(350, 375)
+		Me.Name = "MainX"
+		Me.Opacity = 0.75R
+		Me.Padding = New System.Windows.Forms.Padding(1)
+		Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+		Me.Text = "Gexed!"
+		Me.panConts.ResumeLayout(False)
+		Me.panConts.PerformLayout()
+		CType(Me.tbOpac, System.ComponentModel.ISupportInitialize).EndInit()
+		Me.ResumeLayout(False)
+
+	End Sub
+	Friend WithEvents panGex As System.Windows.Forms.Panel
+	Friend WithEvents panConts As System.Windows.Forms.Panel
+	Friend WithEvents txUrl As System.Windows.Forms.TextBox
+	Friend WithEvents tbOpac As System.Windows.Forms.TrackBar
+	Friend WithEvents lbLoad As System.Windows.Forms.Label
+	Friend WithEvents lbBack As System.Windows.Forms.Label
+	Friend WithEvents lbReload As System.Windows.Forms.Label
+	Friend WithEvents lbForward As System.Windows.Forms.Label
+	Friend WithEvents butMin As System.Windows.Forms.Button
+	Friend WithEvents lbUA As System.Windows.Forms.Label
 End Class
--- Gexfox/Forms/MainX.vb
+++ Gexfox/Forms/MainX.vb
@@ -1,3 +1,121 @@
 ﻿Public Class MainX
+	Dim GeX As New Gecko.GeckoWebBrowser
+
+	Private Async Sub MainX_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+		Me.Text = "GexFox v" & My.Application.Info.Version.ToString
+		Await Task.Delay(1225)
+
+		'init Xpcom
+		Gecko.Xpcom.Initialize("Firefox")
+
+		'set Geck
+		Gecko.GeckoPreferences.User("general.useragent.override") = "Mozilla/5.0 (Android 4.4; Mobile; rv:41.0) Gecko/41.0 Firefox/41.0"
+
+		'GeX Settings
+		With GeX
+			.Dock = DockStyle.Fill
+		End With
+		'Add to Panel
+		panGex.Controls.Add(GeX)
+
+		GeX.Navigate("about:blank")
+
+		lbLoad.Visible = False
+	End Sub
+
+	Private Sub MainX_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+		Me.Hide()
+		GeX.Dispose()
+		Gecko.CookieManager.RemoveAll()
+		Gecko.Xpcom.Shutdown()
+	End Sub
+
+#Region "Controls"
+
+	Private Sub tbOpac_Scroll(sender As Object, e As EventArgs) Handles tbOpac.Scroll
+		Me.Opacity = tbOpac.Value / 100
+
+		If panGex.Visible Then
+			GeX.Focus()
+		End If
+	End Sub
+
+	Private Sub txUrl_KeyDown(sender As Object, e As KeyEventArgs) Handles txUrl.KeyDown
+		If e.KeyCode = Keys.Enter And Not lbLoad.Visible Then
+			Dim newAdd As String = txUrl.Text
+			If Not String.IsNullOrWhiteSpace(newAdd) Then
+				Dim urx As Uri = Nothing
+				Try
+					urx = New Uri("http://" & newAdd.Trim)
+				Catch ex As Exception
+					urx = Nothing
+				End Try
+
+				lbLoad.Visible = True
+				If Not IsNothing(urx) Then
+					GeX.Navigate(urx.DnsSafeHost)
+				Else
+					GeX.Navigate("https://www.google.com/search?q=" & Replace(newAdd, " ", "+") & "&oq=" & Replace(newAdd, " ", "+"))
+				End If
+				lbLoad.Visible = False
+			End If
+		End If
+	End Sub
+
+	Private Sub lbReload_Click(sender As Object, e As EventArgs) Handles lbReload.Click
+		If lbLoad.Visible Then
+			Exit Sub
+		End If
+		lbLoad.Visible = True
+		GeX.Reload()
+		lbLoad.Visible = False
+	End Sub
+
+	Private Sub lbBack_Click(sender As Object, e As EventArgs) Handles lbBack.Click
+		If lbLoad.Visible Then
+			Exit Sub
+		End If
+		lbLoad.Visible = True
+		GeX.GoBack()
+		lbLoad.Visible = False
+	End Sub
+
+	Private Sub lbForward_Click(sender As Object, e As EventArgs) Handles lbForward.Click
+		If lbLoad.Visible Then
+			Exit Sub
+		End If
+		lbLoad.Visible = True
+		GeX.GoForward()
+		lbLoad.Visible = False
+	End Sub
+
+	Private Sub butMin_Click(sender As Object, e As EventArgs) Handles butMin.Click
+		Me.WindowState = FormWindowState.Minimized
+	End Sub
+
+	Private Sub lbUA_Click(sender As Object, e As EventArgs) Handles lbUA.Click
+		If lbLoad.Visible Then
+			Exit Sub
+		End If
+
+		If lbUA.Text = "M" Then
+			Gecko.GeckoPreferences.User("general.useragent.override") = "Mozilla/5.0 (Windows NT 6.3; rv:10.0) Gecko/20100101 Firefox/10.0"
+			lbUA.Text = "D"
+		Else
+			Gecko.GeckoPreferences.User("general.useragent.override") = "Mozilla/5.0 (Android 4.4; Mobile; rv:41.0) Gecko/41.0 Firefox/41.0"
+			lbUA.Text = "M"
+		End If
+
+		lbReload_Click(sender, Nothing)
+	End Sub
+
+#End Region
+
+	Private Sub lbLoad_VisibleChanged(sender As Object, e As EventArgs) Handles lbLoad.VisibleChanged
+		panGex.Visible = Not lbLoad.Visible
+		If panGex.Visible Then
+			GeX.Focus()
+		End If
+	End Sub
 
 End Class
