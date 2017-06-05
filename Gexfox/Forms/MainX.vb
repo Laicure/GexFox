c4475622029dfc7d13168ebf8d66c0d566a5691a
Laicure <Laicure@outlook.com>
2017-06-06 07:18:09 +0800

    Revert "major fixes and tweaks!"
    This reverts commit 362f5261a11db5d25121b6c3a9794da1661d4ff2.

--- Gexfox/Forms/MainX.vb
+++ Gexfox/Forms/MainX.vb
@@ -1,37 +1,17 @@
 ﻿Public Class MainX
 	Dim GeX As New Gecko.GeckoWebBrowser
-	Dim MobileUA As String = "Mozilla/5.0 (Mobile; rv:45.0.31) Gecko/45.0.31 Firefox/45.0.31"
-	Dim DesktopUA As String = "Mozilla/5.0 (Windows NT 6.3; rv:45.0.31) Gecko/20100101 Firefox/45.0.31"
+	Dim MobileUA As String = "Mozilla/5.0 (Android 4.4; Mobile; rv:41.0) Gecko/41.0 Firefox/41.0"
+	Dim DesktopUA As String = "Mozilla/5.0 (Windows NT 6.3; rv:10.0) Gecko/20100101 Firefox/10.0"
 
 	Private Async Sub MainX_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 		Me.Text = "GexFox v" & My.Application.Info.Version.ToString
-		Me.Icon = My.Resources.meteor 'materialdesignicons.com
-		notIGexed.Icon = My.Resources.meteor 'materialdesignicons.com
-		notIGexed.Text = "GexFox v" & My.Application.Info.Version.ToString
-
 		Await Task.Delay(1225)
 
 		'init Xpcom
 		Gecko.Xpcom.Initialize("Firefox")
 
-		'Event Mapping
-		AddHandler GeX.CreateWindow, AddressOf Gex_CreateWindow
-		AddHandler GeX.NavigationError, AddressOf Gex_NavigationError
-		AddHandler GeX.Navigating, AddressOf Gex_Navigating
-		AddHandler GeX.Navigated, AddressOf Gex_Navigated
-		AddHandler GeX.DomKeyDown, AddressOf Gex_KeyDown
-
 		'set Geck inits
 		Gecko.GeckoPreferences.User("general.useragent.override") = MobileUA
-		Gecko.GeckoPreferences.User("zoom.maxPercent") = 100
-		Gecko.GeckoPreferences.User("zoom.minPercent") = 100
-		Gecko.GeckoPreferences.User("layout.spellcheckDefault") = 2
-		Gecko.GeckoPreferences.User("ui.SpellCheckerUnderlineStyle") = 4
-		Gecko.GeckoPreferences.User("network.http.pipelining") = True
-		Gecko.GeckoPreferences.User("network.prefetch-next") = False
-		Gecko.GeckoPreferences.User("dom.max_script_run_time") = 17
-		Gecko.GeckoPreferences.User("browser.backspace_action") = 1
-		Gecko.GeckoPreferences.User("general.warnOnAboutConfig") = False
 
 		'GeX Settings
 		With GeX
@@ -40,61 +20,22 @@
 		'Add to Panel
 		panGex.Controls.Add(GeX)
 
-		GeX.LoadHtml("<html><body bgcolor=""#FFFFFF""><h6>Ready!</h6></body></html>", Nothing)
+		GeX.Navigate("about:blank")
+
+		lbLoad.Visible = False
 	End Sub
 
 	Private Sub MainX_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
 		Me.Hide()
-
 		GeX.Dispose()
 		Gecko.CookieManager.RemoveAll()
 		Gecko.Xpcom.Shutdown()
-
-		'delete Geckofx folder
-		If My.Computer.FileSystem.DirectoryExists("C:\Users\" & Environment.UserName & "\AppData\Local\Geckofx") Then
-			Try
-				My.Computer.FileSystem.DeleteDirectory("C:\Users\" & Environment.UserName & "\AppData\Local\Geckofx", FileIO.DeleteDirectoryOption.DeleteAllContents)
-			Catch ex As Exception
-				Console.WriteLine(Err.Source & ": " & Err.Description)
-			End Try
-		End If
 	End Sub
-
-#Region "Gex Events"
-
-	Private Sub Gex_Navigated(sender As Object, e As Gecko.GeckoNavigatedEventArgs)
-		lbLoad.Visible = False
-	End Sub
-
-	Private Sub Gex_Navigating(sender As Object, e As Gecko.Events.GeckoNavigatingEventArgs)
-		lbLoad.Visible = True
-	End Sub
-
-	Private Sub Gex_NavigationError(sender As Object, e As Gecko.Events.GeckoNavigationErrorEventArgs)
-		'GeX.LoadHtml("<html><body bgcolor=""#FFFFFF""><h6>Error: " & e.ErrorCode.ToString & "</h6></body></html>", Nothing)
-	End Sub
-
-	'prevent new window/tab
-	Private Sub Gex_CreateWindow(sender As Object, e As Gecko.GeckoCreateWindowEventArgs)
-		e.Cancel = True
-		GeX.Navigate(e.Uri)
-		GeX.Focus()
-	End Sub
-
-	'Quick Hide
-	Private Sub Gex_KeyDown(sender As Object, e As Gecko.DomKeyEventArgs)
-		If e.KeyCode = Keys.Escape Then
-			butMin_Click(sender, Nothing)
-		End If
-	End Sub
-
-#End Region
 
 #Region "Controls"
 
 	Private Sub tbOpac_Scroll(sender As Object, e As EventArgs) Handles tbOpac.Scroll
 		Me.Opacity = tbOpac.Value / 100
-		tipper.SetToolTip(tbOpac, "Opacity: " & tbOpac.Value & "%")
 
 		If panGex.Visible Then
 			GeX.Focus()
@@ -105,22 +46,20 @@
 		If e.KeyCode = Keys.Enter And Not lbLoad.Visible Then
 			Dim newAdd As String = txUrl.Text
 			If Not String.IsNullOrWhiteSpace(newAdd) Then
-				If newAdd.Trim = "about:config" Then
-					GeX.Navigate(newAdd)
-				Else
-					Dim urx As Uri = Nothing
-					Try
-						urx = New Uri("http://" & newAdd.Trim)
-					Catch ex As Exception
-						urx = Nothing
-					End Try
+				Dim urx As Uri = Nothing
+				Try
+					urx = New Uri("http://" & newAdd.Trim)
+				Catch ex As Exception
+					urx = Nothing
+				End Try
 
-					If Not IsNothing(urx) Then
-						GeX.Navigate(urx.DnsSafeHost)
-					Else
-						GeX.Navigate("https://www.google.com/search?q=" & Replace(newAdd, " ", "+") & "&oq=" & Replace(newAdd, " ", "+"))
-					End If
+				lbLoad.Visible = True
+				If Not IsNothing(urx) Then
+					GeX.Navigate(urx.DnsSafeHost)
+				Else
+					GeX.Navigate("https://www.google.com/search?q=" & Replace(newAdd, " ", "+") & "&oq=" & Replace(newAdd, " ", "+"))
 				End If
+				lbLoad.Visible = False
 			End If
 		End If
 	End Sub
@@ -129,38 +68,31 @@
 		If lbLoad.Visible Then
 			Exit Sub
 		End If
+		lbLoad.Visible = True
 		GeX.Reload()
-		GeX.Focus()
+		lbLoad.Visible = False
 	End Sub
 
 	Private Sub lbBack_Click(sender As Object, e As EventArgs) Handles lbBack.Click
 		If lbLoad.Visible Then
 			Exit Sub
 		End If
+		lbLoad.Visible = True
 		GeX.GoBack()
-		GeX.Focus()
+		lbLoad.Visible = False
 	End Sub
 
 	Private Sub lbForward_Click(sender As Object, e As EventArgs) Handles lbForward.Click
 		If lbLoad.Visible Then
 			Exit Sub
 		End If
+		lbLoad.Visible = True
 		GeX.GoForward()
-		GeX.Focus()
+		lbLoad.Visible = False
 	End Sub
 
 	Private Sub butMin_Click(sender As Object, e As EventArgs) Handles butMin.Click
-		If Me.Visible Then
-			Me.Hide()
-			notIGexed.Visible = True
-		End If
-		'Me.WindowState = FormWindowState.Minimized
-	End Sub
-
-	Private Sub notIGexed_MouseDoubleClick(sender As Object, e As MouseEventArgs) Handles notIGexed.MouseDoubleClick
-		Me.Show()
-		notIGexed.Visible = False
-		Me.Activate()
+		Me.WindowState = FormWindowState.Minimized
 	End Sub
 
 	Private Sub lbUA_Click(sender As Object, e As EventArgs) Handles lbUA.Click
